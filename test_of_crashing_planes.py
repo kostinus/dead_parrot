76bdb05f873c683308b20d4ed7412308b82eec4a
Konstantin Gusev <kostinus@gmail.com>
2020-08-12 09:13:25 +0300

    Добавлена проверка, что программа запущена не как импортированный модуль. Постарался отформатировать код в стиле PEP8. Добавлена визуализация самолётов. Изменено описание самолётов: сразу задаём скорость,\              направление и актуальное положение. Изменён алгоритм ходов. Вычисляем новое положение, \              проверяем вылет за пределы, \              потом проверяем на пересечение траекторий.

--- test_of_crashing_planes.py
+++ test_of_crashing_planes.py
@@ -1,96 +1,192 @@
-from numpy import arange
-import random
-from tkinter import *
 from math import cos, sin, pi
-from time import sleep
+import random
+import tkinter as tk
+import time
+
+from numpy import arange
 from sympy import Point, Line, Segment
 
-number_of_planes = int(input("Сколько самолётов запустим?"))
 
-class Plane(object):# Описываем самолёт как класс.
-    def __init__ (self, number, x_position, y_position, old_x, old_y, speed, direction):
+if __name__ == "__main__":
+    while True:
+        try:
+            user_value = input("Сколько самолётов запустим?")
+            if int(user_value) > 0:
+                number_of_planes = int(user_value)
+                break
+            elif int(user_value) <= 0:
+                print ("Надо ввести положительное число.")
+        except ValueError:
+                print ("Надо ввести положительное число.")
+
+
+# Описываем самолёт как класс.  
+class Plane(object):
+    def __init__ (self, number, x_position, y_position, new_x_position, new_y_position, speed, direction, colour):
         self.number = number
         self.x_position = x_position
         self.y_position = y_position
-        self.old_x = old_x
-        self.old_y = old_y
+        self.new_x_position = new_x_position
+        self.new_y_position = new_y_position
         self.speed = speed
         self.direction = direction
+        self.colour = colour
 
     def __str__ (self):
-        return "cамолёт №{}, старое положение {}, {}, новое положение {},{}".format(self.number,self.old_x, self.old_y, self.x_position, self.y_position)
+        return "cамолёт №{}, старое положение {}, {}, новое положение {},{}, скорость {}, направление {}".format(self.number,self.x_position, self.y_position, self.new_x_position, self.new_y_position,self.speed,self.direction)
 
-    def fly(self): # Функция, описывающая перемещение самолёта на каждом ходе.
-        # Задаём случайную скорость.
-        self.speed = random.randrange(300,1000)
-        # Случайно изменение направления. Чтобы не накручивать слишком большое количество градусов, контролируем, чтобы итоговое значение осталось в пределах 360.
-        if self.direction >=15 or self.direction <= 345:
+    # Функция, описывающая перемещение самолёта на каждом ходе.  
+    def fly(self):
+        # Вычисляем новое положение.  
+        self.x_position = self.new_x_position
+        self.y_position = self.new_y_position
+        self.new_x_position = round(self.x_position + self.speed * cos(self.direction * pi / 180), 2)
+        self.new_y_position = round(self.y_position + self.speed * sin(self.direction * pi / 180), 2)
+
+        # Случайно изменение направления. Чтобы не накручивать слишком большое количество градусов, контролируем, чтобы итоговое значение осталось в пределах 360.  
+        if self.direction >= 15 or self.direction <= 345:
             self.direction = self.direction + random.randrange(-15,16)
         elif self.direction < 15:
             self.direction = self.direction + random.randrange (0,16)
         else:
             self.direction = self.direction - random.randrange (0,16)
-        # Перед изменением позиции запоминаем предыдущее положение.
-        self.old_x = self.x_position
-        self.old_y = self.y_position
-        # Вычисляем новое положение.
-        self.x_position = round (self.x_position + self.speed * cos(self.direction*pi/180),2)
-        self.y_position = round(self.y_position + self.speed * sin(self.direction*pi/180),2)
+        # Присваиваем новое значение скорости.  
+        self.speed = random.choice(speed_random)
 
-    def out(self): # Функция для определения вылета за пределы поля.
-        if self.x_position > 5000 or self.y_position > 5000 or self.x_position < 0 or self.y_position < 0: 
+    # Функция для определения вылета за пределы поля.  
+    def out(self):
+        if self.new_x_position > 5000 or self.new_y_position > 5000 or self.new_x_position < 0 or self.new_y_position < 0:
             return 1
         else:
             return 0
+    
+    # Визуализация полёта самолёта. Поскольку мне надо, чтобы всё поместилось на экране, рисуем в масштабе 1 к 10. Все значения для визуализации делим на 10.  
+    def visual(self):
+        # Рисуем вектор, где начальная точка – это предыдущее положение, а конечная точка – новое положение.  
+        canvas.create_line(self.x_position/10, self.y_position/10, self.new_x_position/10, self.new_y_position/10, fill = self.colour, arrow="last", tag = "visual_plane")
 
-    def crash(self,other): # Функция для определения столкновения двух самолётов. Мы создаём для каждого самолёта по отрезку от старого положения до нового. Функция возвращает список точек пересечения двух отрезков. Если длина списка нулевая, пересечения нет. Если больше 0, значит пересечение есть и самолёты столкнулись.
-        seg1_1 = Point(self.old_x,self.old_y)
-        seg1_2 = Point(self.x_position,self.y_position)
-        seg2_1 = Point(other.old_x,other.old_y)
-        seg2_2 = Point(other.x_position,other.y_position)
-        seg1 = Segment(seg1_1,seg1_2)
-        seg2 = Segment(seg2_1,seg2_2)
+        # Создаём текстовую метку с номером самолёта, которая будет отображаться рядом с ним.  
+        if self.new_x_position < 4950:
+            text_x = self.new_x_position/10 + 10
+        else:
+            text_x = self.new_x_position/10-10
+        if self.new_y_position/10 < 4950:
+            text_y = (self.new_y_position + 100)/10 + 10
+        else:
+            text_y = self.new_y_position/10 - 10
+        canvas.create_text(text_x, text_y, text = self.number,fill = self.colour, tag = "visual_plane")
+
+    # Функция для определения столкновения двух самолётов. Мы создаём для каждого самолёта по отрезку от старого положения до нового. Функция возвращает список точек пересечения двух отрезков. Если длина списка нулевая, пересечения нет. Если больше 0, значит пересечение есть и самолёты столкнулись.  
+    def crash(self, other):
+        seg1_1 = Point(self.x_position, self.y_position)
+        seg1_2 = Point(self.new_x_position,self.new_y_position)
+        seg2_1 = Point(other.x_position, other.y_position)
+        seg2_2 = Point(other.new_x_position, other.new_y_position)
+        seg1 = Segment(seg1_1, seg1_2)
+        seg2 = Segment(seg2_1, seg2_2)
         return len(seg1.intersection(seg2))
 
-pos_random = arange(1,5000,0.1)
-dir_random = range(1,360,1)
+
+# Это функция для случайного выбора цвета самолёта.  
+def plane_colour():
+    hex_digits = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    digit_array = []
+    for m in range(6):
+        digit_array.append(hex_digits[random.randint(0,15)])
+    joined_digits = ''.join(digit_array)
+    colour = '#' + joined_digits
+    return colour
+
+pos_random = arange(1, 5000, 0.1)
+dir_random = range(1, 360, 1)
+speed_random = range(300, 1001, 100)
 planes = list()
 
-for i in range(1,number_of_planes+1): # Создаём самолёты в том количестве, которое получили от пользователя.
-    a = Plane(i,round (random.choice(pos_random),2),round(random.choice(pos_random),2),0,0,0,random.choice(dir_random))
+# Создаём окно визуализации.  
+# Поскольку я хочу, чтобы всё поместилось на экране, отображение будет в масштабе 1 к 10.
+root = tk.Tk()
+root.title("Полетаем?")
+root.attributes("-topmost", True)
+#root.attributes("-fullscreen", True)
+canvas = tk.Canvas(width = 500, height = 500,bg="white") 
+canvas.pack()
+
+# Рисуем сетку.  
+for g in range(0, 500, 50):
+    canvas.create_line(0, g, 1000, g, fill="#A0A0A0")
+    canvas.create_line(g, 0, g, 1000, fill="#A0A0A0")
+    canvas.create_text(g+15, 10, text = g * 10, fill="#A0A0A0", justify="left")
+    if g > 0:
+        canvas.create_text(15, g+10, text = g * 10, fill="#A0A0A0", justify="left")
+    canvas.update()
+
+""" Рисуем обратный отсчёт.
+for c in range(3,0,-1):
+    countdown = canvas.create_text(250,250,text=c,fill="red",font=50)
+    canvas.update()
+    sleep(1)
+    canvas.delete(countdown)
+countdown = canvas.create_text(250, 250, text="полетели!", fill="red",font=50)
+canvas.update()
+sleep(1)
+canvas.delete(countdown) """
+
+# Создаём самолёты в том количестве, которое получили от пользователя.  
+for i in range(1, number_of_planes+1):
+    a = Plane(i, round(random.choice(pos_random), 2), round(random.choice(pos_random), 2), 0, 0, random.choice(speed_random), random.choice(dir_random), plane_colour())
+    a.new_x_position = a.x_position
+    a.new_y_position = a.y_position
+    # print("Создан {}".format(a))
     planes.append(a)
 
-count = 1 # Номер хода.
-
-for f in planes: #Первый полёт.
-    f.fly()
+count = 1       # Номер хода.
 
 while True:
-    print("Ход №{}".format(count))
+    t = time.time()        # Фиксируем время начала хода
 
-    for cr in planes:
-        # Берём каждый самолёт в списке и проверяем на столкновение с каждым последующим из списка.
-        current_plane = cr
-        current_plane_index = planes.index(cr)
-        for an in planes[current_plane_index+1:number_of_planes]:
-            if current_plane.crash(an)>0:
-                another_plane = an
-                another_plane_index = planes.index(another_plane)
-                print("Cтолкнулись самолёты {} и {}.".format(current_plane,another_plane))
-                del planes[another_plane_index]
-                del planes[current_plane_index]
-        
+    # Очищаем экран и рисуем текущее состояние.  
+    canvas.delete("count")
+    canvas.create_text(460, 490, text = "ход № {}".format(count), tag = "count")
+    canvas.delete("visual_plane")
+    for vis in planes:
+        vis.visual()
+    canvas.update()
+    
+    print("Ход №{}".format(count))
+    for f in planes:
+        f.fly()
+        # print("Полетел {}".format(f))
+
+    # Берём каждый самолёт в списке и проверяем, не покинул ли он поле.  
     for off in planes:
+        # print("самолёт {}.off = {}".format(off.number, off.out()))
         if off.out()==1:
             planes.remove(off)
             print("{} покинул поле.".format(off))
 
-    for n in planes:
-        n.fly()
+    # Берём каждый самолёт в списке и проверяем на столкновение с каждым последующим из списка.  
+    for cr in planes:
+        current_plane = cr
+        current_plane_index = planes.index(cr)
+        for an in planes[current_plane_index+1:number_of_planes]:
+            # print("самолёт {}.crash(самолёт {}) = {}".format(cr.number,an.number,cr.crash(an)))
+            if current_plane.crash(an) > 0:
+                another_plane = an
+                another_plane_index = planes.index(an)
+                print("Cтолкнулись самолёты {} и {}.".format(current_plane, another_plane))
+                del planes[another_plane_index]
+                del planes[current_plane_index]
+                break
+    
+    tn = time.time()            # Фиксируем время завершения хода.
+    
+    # Для большей равномерности выполнения, если с начала цикла прошло меньше 3 секунд, выжидаем, пока не пройдут 3 секунды. Иначе – сразу продолжаем.  
+    if int(t - tn) <= 3:
+        time.sleep(3 - int(t - tn))
 
     count = count + 1
 
-    # Если список самолётов опустел, то и мы завершаем.
+    # Если список самолётов опустел, то и мы завершаем.  
     if len(planes) == 0:
         break
 
